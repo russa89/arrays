ff34bc816b45b8998ac1a75ccc6e5b2c6144c07f
russa89 <natast2189@gmail.com>
2024-04-15 15:12:58 +0300

    first task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("\nЗадание 1");
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        System.out.println(Arrays.toString(numbers));
+        // array 1
+
+        double[] numbers1 = {1.57, 7.654, 9.986};
+        System.out.println(Arrays.toString(numbers1));
+        //array 2
+
+        int [] arr = {1, 2, 3, 7, 8, 9};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Arrays.toString(arr));}
+        //array 3
+        System.out.println();
+            System.out.println("Задание 2");
+
+
     }
 }
