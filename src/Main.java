0cbd4ce3a98fa784df585eb94d03b2cc86442f5a
russa89 <natast2189@gmail.com>
2024-04-16 12:56:48 +0300

    homework is done

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("\nЗадание 1");
+        System.out.println("\nЗадания 1, 2");
         int[] numbers = new int[3];
         numbers[0] = 1;
         numbers[1] = 2;
@@ -15,13 +15,50 @@ public class Main {
         System.out.println(Arrays.toString(numbers1));
         //array 2
 
-        int [] arr = {1, 2, 3, 7, 8, 9};
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(Arrays.toString(arr));}
+        int[] arr = {1, 2, 3, 7, 8};
+        System.out.println(Arrays.toString(arr));
         //array 3
         System.out.println();
-            System.out.println("Задание 2");
 
+        System.out.println("Задание 3");
+        for (int i = 0; i < numbers.length; i++) {
+            int a = numbers[0];
+            numbers[0] = numbers[numbers.length - 1];
+            numbers[numbers.length - 1] = a;
+        }
+        System.out.println(Arrays.toString(numbers));
+        //array 1
 
+        for (double i = 0; i < numbers1.length; i++) {
+            double b = numbers1[0];
+            numbers1[0] = numbers1[numbers.length - 1];
+            numbers1[numbers.length - 1] = b;
+        }
+        System.out.println(Arrays.toString(numbers1));
+        //array 2
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+//        //array 3
+        System.out.println();
+
+        System.out.println("Задание 4");
+            for (int i = 0; i < numbers.length; i++) {
+            int a = numbers[0];
+            numbers[0] = numbers[numbers.length - 1];
+            numbers[numbers.length - 1] = a;
+            if (numbers[0] % 2 != 0) {
+                numbers[0] = numbers[0] + 1;
+            }
+        }
+        System.out.println(Arrays.toString(numbers));
     }
 }
+
+
+
+
+
+
+
